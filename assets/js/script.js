d947653a83de8e7b3807bde155fadb2c35d112eb
grimalkin20 <kr.himanshu@outlook.in>
2025-06-08 09:49:18 +0530

    changes in script and style

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
+    document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+        function ctrlShiftKey(e, keyCode) {
+        return e.ctrlKey && e.shiftKey && e.keyCode === keyCode.charCodeAt(0);
+    }
+
+    document.onkeydown = (e) => {
+        // Disable F12, Ctrl + Shift + I, Ctrl + Shift + J, Ctrl + U
+        if (event.keyCode === 123 || ctrlShiftKey(e, 'I') || ctrlShiftKey(e, 'J') || ctrlShiftKey(e, 'C') || (e.ctrlKey && e.keyCode === 'U'.charCodeAt(0))) {
+            return false;
+        }
+    };
 
 setTimeout(function() {
     var preloader = document.getElementById('preloader');
@@ -24,3 +36,18 @@ document.addEventListener('DOMContentLoaded', function () {
                                 });
                             }
                         });
+
+
+
+// Show/hide scroll to top button
+            const scrollBtn = document.getElementById('scrollToTopBtn');
+            window.addEventListener('scroll', function() {
+            if (window.scrollY > 200) {
+                scrollBtn.style.display = 'block';
+            } else {
+                scrollBtn.style.display = 'none';
+            }
+            });
+            scrollBtn.addEventListener('click', function() {
+            window.scrollTo({ top: 0, behavior: 'smooth' });
+            });
